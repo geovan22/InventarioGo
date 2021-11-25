e7a84aec6f05fa7f4b5d0f16196644811a93db6a
geovan22 <geovan221315@hotmail.com>
2021-11-25 01:53:27 +0100

    tpl
    tpl

--- templates_c/da1209a6a890222b401b6d95381f59ecb1aed01d_0.file.Inventario.tpl.php
+++ templates_c/da1209a6a890222b401b6d95381f59ecb1aed01d_0.file.Inventario.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.40, created on 2021-11-16 03:23:40
+/* Smarty version 3.1.40, created on 2021-11-23 01:55:08
   from 'C:\xampp\htdocs\InventarioGo\templates\Inventario\Inventario.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.40',
-  'unifunc' => 'content_6193162cad2733_81442817',
+  'unifunc' => 'content_619c3becc6a169_56334501',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'da1209a6a890222b401b6d95381f59ecb1aed01d' => 
     array (
       0 => 'C:\\xampp\\htdocs\\InventarioGo\\templates\\Inventario\\Inventario.tpl',
-      1 => 1637029417,
+      1 => 1637290776,
       2 => 'file',
     ),
   ),
@@ -22,10 +22,9 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:Inventario/".((string)$_smarty_tpl->tpl_vars[\'Invent\']->value).".tpl' => 1,
   ),
 ),false)) {
-function content_6193162cad2733_81442817 (Smarty_Internal_Template $_smarty_tpl) {
+function content_619c3becc6a169_56334501 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender('file:Navs/NavLateral.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
-
 
 <div class="col s10">
     <?php if ((isset($_smarty_tpl->tpl_vars['Invent']->value))) {?>
@@ -34,5 +33,10 @@ $_smarty_tpl->_subTemplateRender('file:Navs/NavLateral.tpl', $_smarty_tpl->cache
     <?php } else { ?>
         <img class="responsive-img" src="Recursos/descargar.jpg"/>
     <?php }?>
-</div><?php }
+</div>
+
+<?php echo '<script'; ?>
+ type="text/javascript">
+<?php echo '</script'; ?>
+><?php }
 }
